371e044a7b607ae3292b29dfdf75e9d6db87cc90
Archana0239 <archanaerra2005@gmail.com>
2026-03-05 23:34:28 +0530

    Add files via upload

---
+++ Swayam_Assignment1/ArithmeticOperations.py
@@ -0,0 +1,9 @@
+a = float(input())
+b = float(input())
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+if b != 0:
+    print("Division:", a / b)
+else:
+    print("Division: Undefined")
