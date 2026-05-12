a868f4d4d1a56a7b9dcfb669fd1651e8c9398cca
Archana0239 <archanaerra2005@gmail.com>
2026-05-12 21:05:27 +0530

    Delete Swayam_Assignment1/ArithmeticOperations.py

--- Swayam_Assignment1/ArithmeticOperations.py
+++
@@ -1,9 +0,0 @@
-a = float(input())
-b = float(input())
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-if b != 0:
-    print("Division:", a / b)
-else:
-    print("Division: Undefined")
